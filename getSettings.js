15d09ce6358c7c65dbdf36dcc82772e1ec5bdaa1
Vini Dalvino <37189791+ViniDalvino@users.noreply.github.com>
2021-04-22 11:15:01 -0400

    Fixed bugs and error

--- getSettings.js
+++ getSettings.js
@@ -1,11 +1,16 @@
 module.exports = function () {
+  if (!window.localStorage.getItem("excluded-server"))
+    window.localStorage.setItem("excluded-server", "[]");
+  if (!window.localStorage.getItem("excluded-channel"))
+    window.localStorage.setItem("excluded-channel", "[]");
+  if (!window.localStorage.getItem("excluded-channel"))
   /** @type {string[]} */
-  const excludedServer = window.localStorage.getItem("excluded-server")
-    /** @type {string[]} */
-    const excludedChannel = window.localStorage.getItem("excluded-channel")
+  const excludedServer = window.localStorage.getItem("excluded-server") ?? [];
+  /** @type {string[]} */
+  const excludedChannel = window.localStorage.getItem("excluded-channel") ?? [];
 
   return {
-    maxEmotePerWord: window.localStorage.getItem("max-emoji-per-word"),
+    maxEmotePerWord: window.localStorage.getItem("max-emoji-per-word") ?? 2,
     exludedServer: excludedServer, exludedChannel: excludedChannel
-  }
+  };
 }
--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const getSettings = require("./getSettings");
 
 module.exports = class Emojifier extends Plugin {
   async startPlugin() {
-    this.loadStylesheet("style.scss");
+    // set the config if none is set
+    if (!window.localStorage.getItem("excluded-server"))
+      window.localStorage.setItem("excluded-server", "[]");
+    if (!window.localStorage.getItem("excluded-channel"))
+      window.localStorage.setItem("excluded-channel", "[]");
+    if (!window.localStorage.getItem("excluded-channel"))
+      this.loadStylesheet("style.scss");
+      
     powercord.api.settings.registerSettings("emojify", {
       category: this.entityID,
       label: "Emojifier",
@@ -18,30 +25,29 @@ module.exports = class Emojifier extends Plugin {
     setImmediate(async () => {
       const messageEvents = await getModule(["sendMessage"], true);
       if (!messageEvents)
-        throw new ReferenceError("Failed to load message events")
-
-      inject("emojifier", messageEvents, "sendMessage", function (args) {
+        throw new ReferenceError("Failed to load message events");
+      inject("emojifier-injection-id", messageEvents, "sendMessage", function (args) {
         const { getChannel } = getModule(["getChannel"], false);
-        const emojiPasta = generateEmojipasta(args[1].content)
+        const emojiPasta = generateEmojipasta(args[1].content);
         const channel = getChannel(args[0]);
-        const { exludedChannel, exludedServer} = getSettings()
-        const excludedChannel = exludedChannel.map(e => e.split(/(\d+)/)[1])
-        const excludedServer = exludedServer.map(s => s.split(/(\d+)/)[1])
-        const isUserChannel = channel.type === 1
+        const { exludedChannel, exludedServer } = getSettings();
+        const excludedChannel = exludedChannel.map(e => e.split(/(\d+)/)[1]);
+        const excludedServer = exludedServer.map(s => s.split(/(\d+)/)[1]);
+        const isUserChannel = channel.type === 1;
 
         if (excludedChannel.includes(channel.id) || excludedServer.includes(channel.guild_id))
-          return args
+          return args;
         // block emote from dm
         for (const exclChannel of excludedChannel) {
           if (channel.recipients.includes(exclChannel))
-            return args
+            return args;
         }
 
         for (const server of excludedServer) {
           if (channel.recipients.includes(server))
-            return args
+            return args;
         }
-        args[1].content = emojiPasta
+        args[1].content = emojiPasta;
         return args;
       });
     });
@@ -51,6 +57,6 @@ module.exports = class Emojifier extends Plugin {
   pluginWillUnload() {
     powercord.api.settings.unregisterSettings("emojify");
     powercord.api.settings.unregisterCommand("emojify");
-    uninject("emojifier");
+    uninject("emojifier-injection-id");
   }
 };
